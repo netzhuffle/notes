69a048c25c47f79d7fba2563edb21992fcc2d431
Jannis Grimm <j@jannis.rocks>
2023-11-03 00:41:07 +0000

    Add NoteController
    For /api/v1/notes REST endpoints

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Return a listing of the user’s notes.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created note in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified note.
+     */
+    public function show(Note $note)
+    {
+        //
+    }
+
+    /**
+     * Update the specified note in storage.
+     */
+    public function update(Request $request, Note $note)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified note from storage.
+     */
+    public function destroy(Note $note)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('v1/notes', NoteController::class);
 });
