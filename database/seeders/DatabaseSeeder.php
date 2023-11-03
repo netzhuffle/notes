4343f7c26647d9c478ac31e119cba955f32107bc
Jannis Grimm <j@jannis.rocks>
2023-11-03 00:18:54 +0000

    Create Note
    - Table
    - Model
    - Relations
    - Factory
    - Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(3)->create();
+        \App\Models\User::factory()
+            ->hasNotes(10)
+            ->create();
+        \App\Models\User::factory()
+            ->create();
+        \App\Models\User::factory()
+            ->hasNotes(2000)
+            ->create();
     }
 }
