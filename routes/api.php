688bbc21618c13edb164d9144d6fd5bfe5c34c2f
Jannis Grimm <j@jannis.rocks>
2023-11-07 18:59:07 +0000

    Add route to create test authentication token

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
 use App\Http\Controllers\NoteController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('v1/notes', NoteController::class);
+});
+
+// This route is to enable easy testing bypassing user registration and login.
+Route::post('v1/users/{user}/token', function (User $user) {
+    $token = $user->createToken('test-token');
+
+    return ['token' => $token->plainTextToken];
 });
